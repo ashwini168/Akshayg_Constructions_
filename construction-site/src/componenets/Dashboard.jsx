be80678d1170243090c4100adee48966a1b0d585
ashwini168 <guddadamaniashwini@gmail.com>
2025-06-19 14:18:20 +0530

    changed

---
+++ construction-site/src/componenets/Dashboard.jsx
@@ -0,0 +1,22 @@
+// src/components/Dashboard.jsx
+const Dashboard = () => (
+    <section id="dashboard" className="section">
+      <h2>Dashboard Overview</h2>
+      <div className="card-grid">
+        <div className="card">
+          <h3>Total Projects</h3>
+          <p>85</p>
+        </div>
+        <div className="card">
+          <h3>Ongoing</h3>
+          <p>12</p>
+        </div>
+        <div className="card">
+          <h3>Completed</h3>
+          <p>73</p>
+        </div>
+      </div>
+    </section>
+  )
+  
+  export default Dashboard;
